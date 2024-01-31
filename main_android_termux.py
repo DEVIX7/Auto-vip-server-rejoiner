8478201c342da3c54fc790494b475436ef9fa676
Developer iX7 <78102400+DEVIX7@users.noreply.github.com>
2024-02-01 00:55:40 +0200

    1.3.3

--- main_android_termux.py
+++ main_android_termux.py
@@ -13,40 +13,54 @@ def colored_text(text, color):
     }
     return f"{colors[color]}{text}{colors['reset']}"
 
+with open('./config.json', 'r') as config_file:
+    config = json.load(config_file)
+    ip = config.get('ip', 'localhost')
+    port = config.get('port', 8126)
+    msglink = config.get('msglink', 'false')
+
 try:
     import websockets
 except ImportError:
     print(colored_text("Module 'websockets' not found. Auto-installing...", 'warn'))
     subprocess.run(['pip', 'install', 'websockets'])
 
-import websockets
+def kill_roblox():
+    pid_result = subprocess.run(['pgrep', '-o', '-f', 'com.roblox.client'], stdout=subprocess.PIPE, text=True)
+    pid = pid_result.stdout.strip()
+
+    print(colored_text("Closing application roblox...", 'warn'))
+
+    if pid:
+        subprocess.run(['kill', pid])
+
+    time.sleep(5)
 
 async def handle_client(websocket, path):
     try:
         async for message in websocket:
-            #print(f"Received message from client: {message}")
+            print(f"Received message from client: {message}")
+            
+            if config.get('msglink') == 'true':
+
+                kill_roblox()
+
+                print(colored_text("Opening link...", 'info'))
+                subprocess.run(['termux-open-url', message])
 
             if message == 'connect-to-vip-server':
 
-                pid_result = subprocess.run(['pgrep', '-o', '-f', 'com.roblox.client'], stdout=subprocess.PIPE, text=True)
-                pid = pid_result.stdout.strip()
+                kill_roblox()
 
-                print(colored_text("Closing application roblox...", 'warn'))
-
-                if pid:
-                    subprocess.run(['kill', pid])
-
-                time.sleep(5)
-
-                with open('./config.json', 'r') as config_file:
-                    config = json.load(config_file)
-                    link = config.get('link', '')
+                link = config.get('link', '')
 
                 print(colored_text("Opening link...", 'info'))
                 subprocess.run(['termux-open-url', link])
 
     except websockets.exceptions.ConnectionClosedError:
         print(f"Client disconnected: {websocket.remote_address}")
+    except Exception as e:
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
     subprocess.run(['clear'])
@@ -58,11 +72,12 @@ if __name__ == "__main__":
      github.com/DEVIX7
     ''', 'bright_cyan'))
 
-    print(colored_text("Auto vip server rejoiner \" version 1.2.1 (android termux)\n", 'info'))
+    print(colored_text("Auto vip server rejoiner \" version 1.3.3 (android termux)\n", 'info'))
 
-    server = websockets.serve(handle_client, "localhost", 8126)
+    server = websockets.serve(handle_client, ip, port)
 
-    print(colored_text("WebSocket server started. Listening on ws://localhost:8126", 'success'))
+    print(colored_text(f"WebSocket server started. Listening on ws://{ip}:{port}", 'success'))
 
     asyncio.get_event_loop().run_until_complete(server)
     asyncio.get_event_loop().run_forever()
+
